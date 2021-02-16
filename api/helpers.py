52c167ccee4b1f9860ab52571bf5e5ff0113cc75
leshawn-rice <leshawnrice3@gmail.com>
2021-02-16 00:07:11 -0800

    Finished helpers.py tests

--- api/helpers.py
+++ api/helpers.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from sqlalchemy import func
 
+# types of acceptable params for helper funcs that check validity of params
+PARAM_TYPES = [int, str]
+
 
 def check_manuf_name(name: str = None):
     '''
@@ -23,7 +26,7 @@ def check_limit(limit: str = None):
     checks if it can be converted to an integer,
     and returns True/False based on that
     '''
-    if not limit or not type(limit) == str:
+    if not type(limit) in PARAM_TYPES:
         return False
     try:
         limit = int(limit)
@@ -37,9 +40,12 @@ def convert_manuf_id(id: str = None):
     Converts id into either an int or None
     and returns it
     '''
+    # If invalid type, return None
+    if not type(id) in PARAM_TYPES:
+        return None
     try:
         id = int(id)
-    except TypeError:
+    except ValueError:
         id = None
     return id
 
@@ -59,15 +65,23 @@ def convert_to_date(date_str: str = None):
         raw_date = datetime.strptime(date_str, '%b %d, %Y')
     except ValueError:
         try:
-            # January, 2000
-            raw_date = datetime.strptime(date_str, '%B, %Y')
+            # January 21, 2000
+            raw_date = datetime.strptime(date_str, '%B %d, %Y')
         except ValueError:
             try:
-                # January 2000
-                raw_date = datetime.strptime(date_str, '%B %Y')
+                # January, 2000
+                raw_date = datetime.strptime(date_str, '%B, %Y')
             except ValueError:
-                # 2000
-                raw_date = datetime.strptime(date_str, '%Y')
+                try:
+                    # January 2000
+                    raw_date = datetime.strptime(date_str, '%B %Y')
+                except ValueError:
+                    try:
+                        # 2000
+                        raw_date = datetime.strptime(date_str, '%Y')
+                    except ValueError:
+                        # Incomprehensible date, return "null" date
+                        return datetime.strptime('January 1900', '%B %Y').date()
 
     date = raw_date.date()
     return date
@@ -78,6 +92,9 @@ def make_date_valid(date_str: str = None):
     Turns invalid phonearena dates into dates that can be converted
     into datetime dates
     '''
+    if not date_str or type(date_str) != str:
+        return None
+
     if '(Official)' in date_str:
         date_str = date_str.replace('(Official)', '')
         date_str = date_str.strip()
@@ -87,12 +104,13 @@ def make_date_valid(date_str: str = None):
     if 'Q2' in date_str:
         date_str = date_str.replace('Q2', 'April')
     if 'Q3' in date_str:
-        date_str = date_str.replace('Q2', 'July')
+        date_str = date_str.replace('Q3', 'July')
     if 'Q4' in date_str:
-        date_str = date_str.replace('Q2', 'October')
+        date_str = date_str.replace('Q4', 'October')
 
+    # Used for announced devices but not released
     if 'Yes' in date_str:
-        # Low effort form phonearena, lower effort from me
+        # Low effort from phonearena, lower effort from me
         date_str = 'January 1900'
 
     return date_str
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from app.app import app
 from app.database import db
 from api.models import Manufacturer
-from api.helpers import check_manuf_name, check_limit
+from api.helpers import check_manuf_name, check_limit, convert_manuf_id, convert_to_date, make_date_valid
 
 app.config['TESTING'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///grabaphone_test-db'
@@ -77,15 +77,14 @@ class CheckLimitTestCase(TestCase):
         is_valid = check_limit('17')
         self.assertTrue(is_valid)
 
+    def test_valid_type_int(self):
+        '''Returns True: valid limit of type int'''
+        is_valid = check_limit(21)
+        self.assertTrue(is_valid)
+
     def test_invalid_limit(self):
         '''Returns False: invalid limit'''
         is_valid = check_limit('NaN')
-        self.assertFalse(is_valid)
-
-    def test_invalid_type_int(self):
-        '''Returns False: valid limit of type int'''
-        # The func is specifically for checking string conversion
-        is_valid = check_limit(21)
         self.assertFalse(is_valid)
 
     def test_invalid_type_bool(self):
@@ -104,3 +103,126 @@ class CheckLimitTestCase(TestCase):
         '''Returns False: no args'''
         is_valid = check_limit()
         self.assertFalse(is_valid)
+
+
+class ConvertManufIdTestCase(TestCase):
+    '''convert_manuf_id Test Case'''
+
+    def test_valid_id(self):
+        '''Returns True: valid limit'''
+        converted_id = convert_manuf_id('1')
+        self.assertEqual(converted_id, 1)
+
+    def test_valid_type_int(self):
+        '''Returns True: valid limit of type int'''
+        converted_id = convert_manuf_id(2)
+        self.assertEqual(converted_id, 2)
+
+    def test_invalid_limit(self):
+        '''Returns False: invalid limit'''
+        converted_id = convert_manuf_id('NaN')
+        self.assertIsNone(converted_id)
+
+    def test_invalid_type_bool(self):
+        '''Returns False: boolean'''
+        converted_id = convert_manuf_id(True)
+        self.assertIsNone(converted_id)
+        converted_id = convert_manuf_id(False)
+        self.assertIsNone(converted_id)
+
+    def test_invalid_type_none(self):
+        '''Returns False: nonetype'''
+        converted_id = convert_manuf_id(None)
+        self.assertIsNone(converted_id)
+
+    def test_invalid_type_no_args(self):
+        '''Returns False: no args'''
+        converted_id = convert_manuf_id()
+        self.assertIsNone(converted_id)
+
+
+class ConvertToDateTestCase(TestCase):
+    '''convert_to_date Test Case'''
+
+    def test_valid_dates(self):
+        '''Returns valid date: valid str'''
+        from datetime import date
+        dates_to_check = ['January 8, 1984', 'Dec 9, 2000',
+                          'December 2020', 'March, 2021', '2010']
+        for raw_date in dates_to_check:
+            converted_date = convert_to_date(raw_date)
+            self.assertIsInstance(converted_date, date)
+            # Regex matches XXXX-XX-XX i.e. 2020-08-21
+            self.assertRegex(f'{converted_date}',
+                             r'^[0-9]{4}-[0-9]{2}-[0-9]{2}$')
+
+    def test_invalid_str(self):
+        '''Returns January 1900: invalid str'''
+        from datetime import date
+        converted_date = convert_to_date('not-a-date')
+        self.assertIsInstance(converted_date, date)
+        # Regex matches XXXX-XX-XX i.e. 2020-08-21
+        self.assertEqual(f'{converted_date}', '1900-01-01')
+
+    def test_no_date(self):
+        '''Returns January 1900: no param'''
+        from datetime import date
+        converted_date = convert_to_date()
+        self.assertIsInstance(converted_date, date)
+        # Regex matches XXXX-XX-XX i.e. 2020-08-21
+        self.assertEqual(f'{converted_date}', '1900-01-01')
+
+
+class MakeDateValidTestCase(TestCase):
+    '''make_date_valid Test Case'''
+
+    def test_official(self):
+        '''Removes (Official) from date_str'''
+
+        valid_str = make_date_valid('(Official) January 2021')
+        self.assertEqual(valid_str, 'January 2021')
+
+    def test_q1(self):
+        '''Replaces Q1 with January in date_str'''
+
+        valid_str = make_date_valid('Q1 2021')
+        self.assertEqual(valid_str, 'January 2021')
+
+    def test_q2(self):
+        '''Replaces Q2 with April in date_str'''
+
+        valid_str = make_date_valid('Q2 2021')
+        self.assertEqual(valid_str, 'April 2021')
+
+    def test_q3(self):
+        '''Replaces Q3 with July in date_str'''
+
+        valid_str = make_date_valid('Q3 2021')
+        self.assertEqual(valid_str, 'July 2021')
+
+    def test_q4(self):
+        '''Replaces Q4 with October in date_str'''
+
+        valid_str = make_date_valid('Q4 2021')
+        self.assertEqual(valid_str, 'October 2021')
+
+    def test_yes(self):
+        '''Replaces Yes with January 1900 in date_str'''
+
+        valid_str = make_date_valid('Yes')
+        self.assertEqual(valid_str, 'January 1900')
+
+    def test_no_data(self):
+        '''Returns None with no params'''
+
+        self.assertIsNone(make_date_valid())
+
+    def test_invalid_type_int(self):
+        '''Returns None with invalid type int'''
+
+        self.assertIsNone(make_date_valid(7))
+
+    def test_invalid_type_int(self):
+        '''Returns None with invalid type bool'''
+
+        self.assertIsNone(make_date_valid(True))
