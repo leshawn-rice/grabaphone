50232e1b96fd8a122d599d29f28952ad7fb20cc3
Leshawn Rice <leshawnrice3@gmail.com>
2021-02-18 18:32:12 -0800

    adding CORS

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from app.database import connect_db
 import os
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgresql:///grabaphone-db')
